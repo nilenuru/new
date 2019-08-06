3dce3004c55beda54a8fd86cede18bf377bce672
nilenuru <zagoraevav@gmail.com>
2019-08-06 07:47:26 +0300

    начало положено

--- src/blocks/modules/features/features.js
+++ src/blocks/modules/features/features.js
@@ -1,4 +1,4 @@
-modules.define('footer', ['i-bem-dom'], function(provide, bemDom) {
+modules.define('features', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod: {
